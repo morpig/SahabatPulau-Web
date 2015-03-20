60a0b40f6c3e57a57c5ad288809a4389a9b3374c
Dito W <pigoshoriginal1@gmail.com>
2015-03-20 19:30:11 +0700

    fonally updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 	<!-- Compiled and minified CSS -->
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/css/materialize.min.css">
-	<link rel="stylesheet" href="https://stamplay.com/stylesheets/application.min.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/sahabatpulau.min.css')}}">
 
 
 
@@ -52,9 +52,9 @@
 				<a href="/register" class="btn waves-effect waves-light yellow darken-2" id="start-now-hero"><span class="dark-grey-text">apply now</span></a>
 			</div>
 		</div>
-		<a class="floating-absolute-right btn-floating btn-large waves-effect waves-light yellow darken-2 hide-on-med-and-down" id="scroll-to-animation"><i class="mdi-navigation-expand-more"></i></a>
 	</section>
 
+	<div id="trigger"></div>
 	<section class="hide-on-small-and-down">
 		<h3>
 			<span>WHAT WE DO</span>
@@ -111,6 +111,63 @@
 			</div>
 	</div>
 	</section>
+
+	<!-- INNER CONTAINER END -->
+
+
+	<!-- FOOTER BEGIN -->
+	<div class="interlude" id="footer-interlude" style="background-image: url('https://farm8.staticflickr.com/7411/8775428047_653ee3989e_o_d.jpg'); background-size: cover;">
+		<div class="hero-polygons hide-on-med-and-down"></div>
+	<h3 class="white-text">Come and apply to Sahabat Pulau! </h3>
+	<a href="https://editor.stamplay.com/register?utm_source=stamplay&utm_content=footer_interlude&utm_medium=website" class="waves-effect waves-light btn yellow darken-2" id="start-now-interlude"><span class="dark-grey-text">start now</span></a>
+</div>
+<footer>
+<!-- 	<div class="valign-wrapper container">
+	<div class="valign row">
+	<h4 class="center-align">Stay updated, somenthing new is happening.</h4>
+		<div class="row">
+			<div class="input-field light col s10">
+				<input id="email" type="email" class="validate">
+				<label for="email">Email</label>
+			</div>
+			<div class="input-field col s2">
+				<a class="waves-effect waves-light btn yellow darken-2">subscribe</a>
+			</div>
+		</div>
+	</div>
+</div> -->
+<div id="footer-nav">
+	<div class="container">
+		<div class="row">
+		<div class="col s12 m3">
+		<p>SAHABAT PULAU</p>
+		<ul>
+			<li><a href="/docs/main">Home</a></li>
+			<li><a href="/pricing">Blog</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+		</div>
+		<div class="col s12 m3">
+		<p>Locations</p>
+
+		</div>
+		<div class="col s12 m3">
+		<p>Upcoming Events</p>
+			<ul>
+				<li>null</li>
+			</ul>
+		</div>
+		<div class="col s12 m3">
+			<ul class="inline">
+				<li><a href="https://www.facebook.com/SahabatPulau" target="_blank"><i class="fa fa-facebook fa-lg"></i></li>
+				<li><a href="https://twitter.com/IDSahabatPulau" target="_blank"><i class="fa fa-twitter fa-lg"></i></a>
+</li>
+			</ul>
+		</div>
+		</div>
+	</div>
+</div>
+</footer>
 
 						<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 						<script src="{{ asset('assets/js/sahabatpulau.min.js')}}"></script>
